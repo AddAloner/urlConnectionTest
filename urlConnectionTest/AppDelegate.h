b2932fbb3a2bd9953db6672d7785e58b7bc0873d
Alexey Yachmenov <aloner.brn@gmail.com>
2015-03-29 19:10:02 +0300

    Initial Commit

---
+++ urlConnectionTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  urlConnectionTest
+//
+//  Created by Alexey Yachmenov on 29.03.15.
+//  Copyright (c) 2015 Alexey Yachmenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ urlConnectionTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  urlConnectionTest
+//
+//  Created by Alexey Yachmenov on 29.03.15.
+//  Copyright (c) 2015 Alexey Yachmenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
